bd39502b023313916a4d6ff49ed54a877bc7c105
Tarek Ali <tarekali15@outlook.com>
2020-05-19 13:06:55 -0500

    fixed syntax

--- backend/database-boilerplate.sql
+++ backend/database-boilerplate.sql
@@ -3,4 +3,4 @@ USE DATABASE Omiran;
 
 CREATE TABLE User ( uuid VARCHAR(36) PRIMARY KEY, username TEXT, email VARCHAR(255), password text, profile_picture VARCHAR(2083) );
 
-CREATE TABLE Follows ( uuid VARCHAR(36) NOT NULL FOREIGN KEY REFERENCES User(uuid), user_following VARCHAR(36) NOT NULL FOREIGN KEY REFERENCES User(uuid));
+CREATE TABLE Follows ( uuid VARCHAR(36) NOT NULL, user_following VARCHAR(36) NOT NULL, FOREIGN KEY (uuid) REFERENCES User(uuid), FOREIGN KEY (user_following) REFERENCES User(uuid));
