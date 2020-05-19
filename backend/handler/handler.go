7ae5b927b2e7f74cc328419498535a4925e1da83
Tarek Ali <tarekali15@outlook.com>
2020-05-19 18:28:46 -0500

    psuedo boilerplate that Im just gonna remove eventually

--- backend/handler/handler.go
+++ backend/handler/handler.go
@@ -2,8 +2,13 @@ package handler
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/graphql-go/go"
+	"github.com/graphql-go/graphql"
 )
 
 func GraphQLService(c *gin.Context) {
-
+	fields := graphql.Fields{}
+	rootQuery := graphql.ObjectConfig{}
+	schemaConfig := graphql.SchemaConfig{}
+	schema, err := graphql.NewSchema()
 }
