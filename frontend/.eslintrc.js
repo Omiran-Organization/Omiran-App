d698341e1f01da730667ed5761bf59d026b67680
emilpriver <emil@priver.dev>
2020-05-19 23:10:05 +0200

    Added react-native-web

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -23,5 +23,6 @@ module.exports = {
         "@typescript-eslint"
     ],
     "rules": {
+        'react/jsx-filename-extension': [2, { 'extensions': ['.js', '.jsx', '.ts', '.tsx'] }],
     }
 };
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import * as React from 'react';
+import { StyleSheet, Text, View } from 'react-native-web';
 
-export default function HomePage() {
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    flexGrow: 1,
+    justifyContent: 'center',
+  },
+  link: {
+    color: 'blue',
+  },
+  textContainer: {
+    alignItems: 'center',
+    marginTop: 16,
+  },
+  text: {
+    alignItems: 'center',
+    fontSize: 24,
+    marginBottom: 24,
+  },
+});
+
+export default function Home() {
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
-  )
+    <View style={styles.container}>
+      <Text accessibilityRole="header" style={styles.text}>
+        React Native for Web & Next.js
+      </Text>
+
+      <Text style={styles.link} accessibilityRole="link" href="/alternate">
+        A universal link
+      </Text>
+
+      <View style={styles.textContainer}>
+        <Text accessibilityRole="header" aria-level="2" style={styles.text}>
+          Subheader
+        </Text>
+      </View>
+    </View>
+  );
 }
