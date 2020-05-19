dc568fa1a013934e66984a96692fef33b02a956f
Tarek Ali <tarekali15@outlook.com>
2020-05-19 13:03:35 -0500

    removed error instigator

--- backend/database-boilerplate.sql
+++ backend/database-boilerplate.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE Omiran;
 USE DATABASE Omiran;
 
-CREATE TABLE User ( uuid VARCHAR(36) PRIMARY KEY, username string TEXT, email VARCHAR(255), password text, profile_picture VARCHAR(2083) );
+CREATE TABLE User ( uuid VARCHAR(36) PRIMARY KEY, username TEXT, email VARCHAR(255), password text, profile_picture VARCHAR(2083) );
 
 CREATE TABLE Follows ( uuid VARCHAR(36) NOT NULL FOREIGN KEY REFERENCES User(uuid), user_following VARCHAR(36) NOT NULL FOREIGN KEY REFERENCES User(uuid));
