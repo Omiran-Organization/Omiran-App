33229dfde412e912a3d2734f950456fc53b200f1
Tarek Ali <tarekali15@outlook.com>
2020-05-19 18:36:57 -0500

    formatting

--- backend/handler/handler.go
+++ backend/handler/handler.go
@@ -2,8 +2,6 @@ package handler
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/graphql-go/go"
-	"github.com/graphql-go/graphql"
 )
 
 func GraphQLService(c *gin.Context) {
--- backend/main.go
+++ backend/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"Omiran-App/backend/handler"
+
 	"github.com/gin-gonic/gin"
 )
 
