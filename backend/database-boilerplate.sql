c02b195313dd84c9fce66da2bb884bb4b7c19863
Tarek Ali <tarekali15@outlook.com>
2020-05-19 12:58:36 -0500

    added references

--- backend/database-boilerplate.sql
+++ backend/database-boilerplate.sql
@@ -3,4 +3,4 @@ USE DATABASE Omiran;
 
 CREATE TABLE User ( uuid VARCHAR(36) PRIMARY KEY, username string TEXT, email VARCHAR(255), password text, profile_picture VARCHAR(2083) );
 
-CREATE TABLE Follows ( uuid VARCHAR(36) NOT NULL, user_following VARCHAR(36) NOT NULL );
+CREATE TABLE Follows ( uuid VARCHAR(36) NOT NULL FOREIGN KEY REFERENCES User(uuid), user_following VARCHAR(36) NOT NULL FOREIGN KEY REFERENCES User(uuid));
