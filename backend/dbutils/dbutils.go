706def6be14ad25f3c070ef02d55a63254e7e220
Tarek Ali <tarekali15@outlook.com>
2020-05-19 18:48:18 -0500

    created database abstraction package and column abstraction structs; will add tags to them later

---
+++ backend/dbutils/dbutils.go
@@ -0,0 +1,26 @@
+package dbutils
+
+import (
+	_ "github.com/go-sql-driver/mysql"
+)
+
+type DbConfig struct {
+	User     string `yaml:"user"`
+	Password string `yaml:"password"`
+	Port     uint32 `yaml:"port"`
+}
+
+type User struct {
+	UUID string 
+	Username string
+	Email string
+	Password string
+	Description string
+	ProfilePicture string
+} 
+
+type Follows struct {
+	UUID string
+	UserFollowing string
+}
+
