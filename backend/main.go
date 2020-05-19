fd74e76da456e706f1da436854da42fcc6898201
Tarek Ali <tarekali15@outlook.com>
2020-05-18 21:05:15 -0500

    proper method arg

--- backend/main.go
+++ backend/main.go
@@ -1,11 +1,12 @@
 package main
 
 import (
+	"Omiran-App/backend/handler"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
-	r.GET("/graphql")
+	r.GET("/graphql", handler.GraphQLService)
 	r.Run()
 }
