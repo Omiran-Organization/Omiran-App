65cc3ef6002e4dcf1acbb18940531915afe69635
Tarek Ali <tarekali15@outlook.com>
2020-05-19 19:03:46 -0500

    added util connection/opening function

--- backend/dbutils/dbutils.go
+++ backend/dbutils/dbutils.go
@@ -1,7 +1,18 @@
 package dbutils
 
 import (
+	"fmt"
+	"io/ioutil"
+	"log"
+
+	"github.com/jmoiron/sqlx"
+
 	_ "github.com/go-sql-driver/mysql"
+	"gopkg.in/yaml.v2"
+)
+
+var (
+	DB *sqlx.DB
 )
 
 type DBConfig struct {
@@ -22,4 +33,20 @@ type User struct {
 type Follows struct {
 	UUID          string
 	UserFollowing string
+}
+
+func Open(filename string) {
+	infoStruct := &DBConfig{}
+	file, err := ioutil.ReadFile(filename)
+	if err != nil {
+		log.Fatalf("database info file error: %s\n", err)
+	}
+	err = yaml.Unmarshal(file, infoStruct)
+	if err != nil {
+		log.Fatalf("unmarshalling problem: %s\n", err)
+	}
+	DB, err = sqlx.Connect("mysql", fmt.Sprintf("%s:%s@tcp(localhost:%d)/Omiran", infoStruct.User, infoStruct.Password, infoStruct.Port))
+	if err != nil {
+		log.Fatalf("database connection error: %s\n", err)
+	}
 }
