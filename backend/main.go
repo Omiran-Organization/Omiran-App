a02998e1eff6a9da9d3e8e60e3a7d865cd2f3583
Tarek Ali <tarekali15@outlook.com>
2020-05-19 19:35:52 -0500

    added database opening

--- backend/main.go
+++ backend/main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"Omiran-App/backend/dbutils"
 	"Omiran-App/backend/handler"
 
 	"github.com/gin-gonic/gin"
 )
+
+func init() {
+	dbutils.Open("database-config.yaml")
+}
 
 func main() {
 	r := gin.Default()
