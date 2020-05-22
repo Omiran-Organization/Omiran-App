5fe96ef1ac4b32068299655a7b0d7d67ed8d7b9e
Tarek Ali <tarekali15@outlook.com>
2020-05-22 14:51:05 -0500

    new func

--- backend/handler/handler.go
+++ backend/handler/handler.go
@@ -1,9 +1,15 @@
 package handler
 
 import (
+	"Omiran-App/backend/dbutils"
 	"github.com/gin-gonic/gin"
+	"github.com/graphql-go/graphql"
 )
 
 func GraphQLService(c *gin.Context) {
+
+}
+
+func graphQLSchema(user []dbutils.User, follows []dbutils.Follows) graphql.Schema {
 
 }
