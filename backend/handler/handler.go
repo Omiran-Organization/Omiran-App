9573be827f9d54cf880952e8fabe3f57b3dc72b1
Tarek Ali <tarekali15@outlook.com>
2020-05-22 15:27:32 -0500

    added part of schema

--- backend/handler/handler.go
+++ backend/handler/handler.go
@@ -2,6 +2,7 @@ package handler
 
 import (
 	"Omiran-App/backend/dbutils"
+
 	"github.com/gin-gonic/gin"
 	"github.com/graphql-go/graphql"
 )
@@ -11,5 +12,20 @@ func GraphQLService(c *gin.Context) {
 }
 
 func graphQLSchema(user []dbutils.User, follows []dbutils.Follows) graphql.Schema {
-
+	fields := &graphql.Fields{
+		"Users": &graphql.Field{
+			Type:        graphql.NewList(dbutils.User),
+			Description: "All Users",
+			Resolve: func(params graphql.ResolveParams) (interface{}, error) {
+				return user, nil
+			},
+		},
+		"Follows": &graphql.Field{
+			Type:        graphql.NewList(dbutils.Follows),
+			Description: "All follows",
+			Resolve: func(params graphql.ResolveParams) (interface{}, error) {
+				return follows, nil
+			},
+		},
+	}
 }
