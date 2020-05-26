5bdb7d49352e99c8935fd9819490cc72e938a341
Kai Chang <kaijchang@gmail.com>
2020-05-26 10:04:22 -0700

    control inputs on signup page

--- frontend/src/pages/login.tsx
+++ frontend/src/pages/login.tsx
@@ -1,10 +1,10 @@
 import * as React from "react";
 
-const LoginPage: React.FunctionComponent = () => {
-  const [emailOrUsername, setEmailOrUsername] = React.useState("");
+import PasswordInput from "@/components/input/passwordinput";
 
+const LoginPage: React.FunctionComponent = () => {
+  const [emailAddressOrUsername, setEmailAddressOrUsername] = React.useState("");
   const [password, setPassword] = React.useState("");
-  const [passwordShown, setPasswordShown] = React.useState(false);
 
   return (
     <div className="flex flex-col items-center h-screen w-4/5 md:w-2/5 mx-auto">
@@ -22,38 +22,24 @@ const LoginPage: React.FunctionComponent = () => {
       <input
         className="input w-full mb-3"
         type="text"
-        value={emailOrUsername}
+        value={emailAddressOrUsername}
         onChange={(e: React.ChangeEvent<HTMLInputElement>): void =>
-          setEmailOrUsername(e.target.value)
+          setEmailAddressOrUsername(e.target.value)
         }
         id="email-username-input"
       />
       <label className="text-blue-500 w-full pl-1" htmlFor="password-input">
         Password
       </label>
-      <div className="input flex flex-row w-full mb-3">
-        <input
-          className="w-full"
-          type={passwordShown ? "text" : "password"}
-          value={password}
-          onChange={(e: React.ChangeEvent<HTMLInputElement>): void =>
-            setPassword(e.target.value)
-          }
-          id="password-input"
-        />
-        <div
-          className="text-gray-700 hover:text-gray-800"
-          onClick={(): void => setPasswordShown(!passwordShown)}
-        >
-          {passwordShown ? (
-            // prettier-ignore
-            <svg className="h-6 s-6 my-auto" fill="currentColor" viewBox="0 0 20 20"><path d="M10 12a2 2 0 100-4 2 2 0 000 4z"></path><path d="M.458 10C1.732 5.943 5.522 3 10 3s8.268 2.943 9.542 7c-1.274 4.057-5.064 7-9.542 7S1.732 14.057.458 10zM14 10a4 4 0 11-8 0 4 4 0 018 0z" clipRule="evenodd" fillRule="evenodd"></path></svg>
-          ) : (
-            // prettier-ignore
-            <svg className="h-6 s-6 my-auto" fill="currentColor" viewBox="0 0 20 20"><path d="M3.707 2.293a1 1 0 00-1.414 1.414l14 14a1 1 0 001.414-1.414l-1.473-1.473A10.014 10.014 0 0019.542 10C18.268 5.943 14.478 3 10 3a9.958 9.958 0 00-4.512 1.074l-1.78-1.781zm4.261 4.26l1.514 1.515a2.003 2.003 0 012.45 2.45l1.514 1.514a4 4 0 00-5.478-5.478z" clipRule="evenodd" fillRule="evenodd"></path><path d="M12.454 16.697L9.75 13.992a4 4 0 01-3.742-3.741L2.335 6.578A9.98 9.98 0 00.458 10c1.274 4.057 5.065 7 9.542 7 .847 0 1.669-.105 2.454-.303z"></path></svg>
-          )}
-        </div>
-      </div>
+      <PasswordInput
+        containerProps={{ className: "mb-3" }}
+        inputProps={{
+          value: password,
+          onChange: (e: React.ChangeEvent<HTMLInputElement>): void =>
+            setPassword(e.target.value),
+          id: "password-input",
+        }}
+      />
       <button className="btn btn-blue self-start">Login</button>
       <div className="flex-grow-3" />
     </div>
--- frontend/src/pages/signup.tsx
+++ frontend/src/pages/signup.tsx
@@ -1,38 +1,99 @@
 import * as React from "react";
+import PasswordInput from "@/components/input/passwordinput";
 
-const SignupPage: React.FunctionComponent = () => (
-  <div className="flex flex-col items-center h-screen w-4/5 md:w-2/5 mx-auto">
-    <div className="flex-grow-2" />
-    <h1 className="text-blue-500 text-4xl font-bold leading-none">Omiran</h1>
-    <h3 className="text-blue-500 text-sm mb-2">
-      The Open Source Streaming Platform
-    </h3>
-    <label className="text-blue-500 w-full pl-1" htmlFor="email-input">
-      Email
-    </label>
-    <input className="input w-full mb-1" type="email" id="email-input" />
-    <label className="text-blue-500 w-full pl-1" htmlFor="username-input">
-      Username
-    </label>
-    <input className="input w-full mb-1" type="text" id="username-input" />
-    <label className="text-blue-500 w-full pl-1" htmlFor="password-input">
-      Password
-    </label>
-    <input className="input w-full mb-1" type="password" id="password-input" />
-    <label
-      className="text-blue-500 w-full pl-1"
-      htmlFor="confirm-password-input"
-    >
-      Confirm Password
-    </label>
-    <input
-      className="input w-full mb-2"
-      type="password"
-      id="confirm-password-input"
-    />
-    <button className="btn btn-blue self-start">Signup</button>
-    <div className="flex-grow-3" />
-  </div>
-);
+const emailAddressRegex = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
+
+const SignupPage: React.FunctionComponent = () => {
+  const [emailAddress, setEmailAddress] = React.useState("");
+  const [username, setUsername] = React.useState("");
+
+  const [password, setPassword] = React.useState("");
+  const [passwordConfirmation, setPasswordConfirmation] = React.useState("");
+
+  // Form validation
+  const isEmailAddressValid = emailAddressRegex.test(emailAddress);
+  const isUsernameValid = username.length > 0;
+
+  const isPasswordValid = password.length > 0;
+  const arePasswordsMatching = password == passwordConfirmation;
+
+  const isFormComplete =
+    isEmailAddressValid &&
+    isUsernameValid &&
+    isPasswordValid &&
+    arePasswordsMatching;
+
+  return (
+    <div className="flex flex-col items-center h-screen w-4/5 md:w-2/5 mx-auto">
+      <div className="flex-grow-2" />
+      <h1 className="text-blue-500 text-4xl font-bold leading-none">Omiran</h1>
+      <h3 className="text-blue-500 text-sm mb-2">
+        The Open Source Streaming Platform
+      </h3>
+      <label className="text-blue-500 w-full pl-1" htmlFor="email-input">
+        Email
+      </label>
+      <input
+        className={`input ${
+          !isEmailAddressValid && emailAddress.length > 0 ? "input-error" : ""
+        } w-full mb-1`}
+        type="email"
+        value={emailAddress}
+        onChange={(e: React.ChangeEvent<HTMLInputElement>): void =>
+          setEmailAddress(e.target.value)
+        }
+        id="email-input"
+      />
+      <label className="text-blue-500 w-full pl-1" htmlFor="username-input">
+        Username
+      </label>
+      <input
+        className="input w-full mb-1"
+        type="text"
+        value={username}
+        onChange={(e: React.ChangeEvent<HTMLInputElement>): void =>
+          setUsername(e.target.value)
+        }
+        id="username-input"
+      />
+      <label className="text-blue-500 w-full pl-1" htmlFor="password-input">
+        Password
+      </label>
+      <PasswordInput
+        containerProps={{ className: "mb-1" }}
+        inputProps={{
+          value: password,
+          onChange: (e: React.ChangeEvent<HTMLInputElement>): void =>
+            setPassword(e.target.value),
+          id: "password-input",
+        }}
+      />
+      <label
+        className="text-blue-500 w-full pl-1"
+        htmlFor="password-confirmation-input"
+      >
+        Confirm Password
+      </label>
+      <PasswordInput
+        containerProps={{ className: "mb-1" }}
+        inputProps={{
+          value: passwordConfirmation,
+          onChange: (e: React.ChangeEvent<HTMLInputElement>): void =>
+            setPasswordConfirmation(e.target.value),
+          id: "password-confirmation-input",
+        }}
+      />
+      <div className="flex flex-row items-center w-full">
+        <button
+          className={`btn ${isFormComplete ? "btn-blue" : "btn-disabled"} mr-4`}
+        >
+          Signup
+        </button>
+        <span className="text-red-500">{ !arePasswordsMatching ? "Passwords do not match." : "" }</span>
+      </div>
+      <div className="flex-grow-3" />
+    </div>
+  );
+};
 
 export default SignupPage;
