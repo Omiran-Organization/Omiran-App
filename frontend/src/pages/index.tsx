44fe0158cbd2e7388b122e3759abccf8e7a45e8c
Tarek Ali <tarekali15@outlook.com>
2020-05-26 17:18:02 -0500

    changed description

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -9,9 +9,7 @@ const HomePage: React.FunctionComponent = () => (
     </Head>
     <h1 className="text-blue-500 text-6xl font-bold m-3">Omiran</h1>
     <h3 className="font-normal text-3xl mb-5">
-      A free and open source live stream software aimed to making streams
-      accessible to everyone even with slower internet connections across the
-      globe.
+      An open source live streaming application that is developer-oriented and removes the political overhead and censorship that other streaming platforms exhibit. The application's name, "Omiran", is Yoruba for, "alternative". This application, from a user's perspective can be interpreted as an alternative; an alternative to the harshly controlled and revenue focused platforms. We will also implement a 0% deduction of streamers' subscription money; funded by either github sponsors, or ads.
     </h3>
     <button className="bg-black px-8 py-3 text-xl rounded-lg text-white">
       Go Live
