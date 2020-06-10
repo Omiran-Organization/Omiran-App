790da53d2a314da1c39b09e81c8e82e6af3e6aad
Tarek Ali <tarekali15@outlook.com>
2020-06-10 16:06:13 -0500

    formatting shit

--- backend/handler/handler.go
+++ backend/handler/handler.go
@@ -50,7 +50,7 @@ func AccountCreationHandler(c *gin.Context) {
 	u := uuid.NewV4()
 	userIntermediary := &dbutils.User{UUID: u, Username: c.Request.FormValue("username"), Email: c.Request.FormValue("email"), Password: c.Request.FormValue("password"), Description: c.Request.FormValue("description"), ProfilePicture: c.Request.FormValue(("profile_picture"))}
 
-	//Maybe 500 status code
+	// Maybe 500 status code
 	err := userIntermediary.Create()
 	if err != nil {
 		c.String(400, err.Error())
