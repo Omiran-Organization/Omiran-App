b9c2f308304cc4bb7b0ed1fc68aef2ca0afbb568
SKaralius <k.simonas.k@gmail.com>
2020-06-20 10:14:56 +0300

    Removes redux.

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,13 +1,8 @@
-import { useDispatch } from 'react-redux'
-import { initializeStore } from '../lib/redux'
-import { initializeApollo } from '../lib/apollo'
-import useInterval from '../lib/useInterval'
-import Layout from '../components/Layout'
-import Clock from '../components/Clock'
-import Counter from '../components/Counter'
-import Submit from '../components/Submit'
+import { initializeApollo } from "../lib/apollo";
+import Layout from "../components/Layout";
+import Submit from "../components/Submit";
 import Head from "next/head";
-import gql from 'graphql-tag';
+import gql from "graphql-tag";
 
 // import PostList, {
 //   ALL_POSTS_QUERY,
@@ -16,87 +11,41 @@ import gql from 'graphql-tag';
 
 const HomePage = (props) => {
   // Tick the time every second
-  const dispatch = useDispatch()
-  
-  useInterval(() => {
-    dispatch({
-      type: 'TICK',
-      light: true,
-      lastUpdate: Date.now(),
-    })
-  }, 1000)
-  console.log(props)
   return (
-
-      <div className="main flex flex-col justify-center items-center w-4/5 lg:w-1/2 mx-auto text-center">
-        <Head>
-          <title>Omiran</title>
-        </Head>
-      
-        <h1 className="text-orange-500 text-6xl font-bold m-3">Omiran</h1>
-        <h3 className="text-white text-3xl mb-5">
-          An open source live streaming application that is developer-oriented and
-          removes the political overhead and censorship that other streaming
-          platforms exhibit.
-      </h3>
-
-        <button className="bg-black px-8 py-3 text-xl rounded-lg text-white">
-          Go Live
-    </button>
-
     <Layout>
-        {/* Redux */}
-        <Clock />
-        <Counter />
-        <hr />
-        {/* Apollo */}
-        <Submit />
-        {/* <PostList /> */}
-      </Layout>
-
-      </div>
-   
-
-    
+      <hr />
+      {/* Apollo */}
+      <Submit />
+      {/* <PostList /> */}
+    </Layout>
   );
-}
-  
+};
 
-  export async function getStaticProps() {
-    const reduxStore = initializeStore()
-    const apolloClient = initializeApollo()
-    const { dispatch } = reduxStore
+export async function getStaticProps() {
+  const apolloClient = initializeApollo();
 
-    dispatch({
-      type: 'TICK',
-      light: true,
-      lastUpdate: Date.now(),
-    })
-
-    await apolloClient.query({
+  await apolloClient
+    .query({
       // query: ALL_POSTS_QUERY,
       // variables: allPostsQueryVars,
 
-        query: gql`
-          query Users { 
-            Users {
-                uuid,
-                username,
-            }
+      query: gql`
+        query Users {
+          Users {
+            uuid
+            username
+          }
         }
-        `
-    }).then(result => console.log(`result${JSON.stringify(result)}`)
-    );
-    
+      `,
+    })
+    .then((result) => console.log(`result${JSON.stringify(result)}`));
 
-    return {
-      props: {
-        initialReduxState: reduxStore.getState(),
-        initialApolloState: apolloClient.cache.extract(),
-      },
-      unstable_revalidate: 1,
-    }
-  }
+  return {
+    props: {
+      initialApolloState: apolloClient.cache.extract(),
+    },
+    unstable_revalidate: 1,
+  };
+}
 
-
-export default HomePage
+export default HomePage;
