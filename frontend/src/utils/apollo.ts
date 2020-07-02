49dfbdcf8a11c7df19f563aa530cd02ce2a51c69
Tarek Ali <tarekali15@outlook.com>
2020-07-02 08:35:06 -0500

    getting shit ready for docs

--- frontend/src/utils/apollo.ts
+++ frontend/src/utils/apollo.ts
@@ -11,7 +11,7 @@ function createApolloClient(): ApolloClient<NormalizedCacheObject> {
   return new ApolloClient({
     ssrMode: typeof window === "undefined",
     link: new HttpLink({
-      uri: "http://localhost:8080/graphql",
+      uri: "http://full_app:8080/graphql",
       // uri: process.env.NODE_ENV=="development" ? "http://full_app:8080/graphql":"http://localhost:8080/graphql", // Server URL (must be absolute)
       credentials: "same-origin", // Additional fetch() options like `credentials` or `headers`
     }),
